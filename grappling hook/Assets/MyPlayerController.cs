167ed6e2ef43051c06a80c5db15698ddcf13d6fb
Joseph Arneil <josepharneil@gmail.com>
2021-11-09 20:37:29 +0000

    MOVEMENT

---
+++ grappling hook/Assets/MyPlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : MonoBehaviour
+{
+    // https://craftgames.co/unity-2d-platformer-movement/
+
+    [SerializeField] private float moveScale = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 velocity;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        BetterJump();
+        CheckIfGrounded();
+    }
+
+    private void Move()
+    {
+        velocity.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(velocity.x * moveScale, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
