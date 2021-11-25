c5bb9e34fc191a79c8f3250be8f0b595aac1d08c
andrewklein98 <aklein@qa.com>
2021-11-25 12:32:55 +0000

    Colour Change on health loss
    Health indicator in the UI now gets closer to red when more health is lost

--- grappling hook/Assets/RoguelikeScripts/RL_HealthUI.cs
+++ grappling hook/Assets/RoguelikeScripts/RL_HealthUI.cs
@@ -8,13 +8,18 @@ public class RL_HealthUI : MonoBehaviour
     [SerializeField] private IntVariable playerHealth;
     [SerializeField] private Text displayText;
     private int healthHolder;
+    private Color red = new Color(255, 0, 0);
+    private Color green = new Color(0, 255, 0);
+    private Color orange = new Color(255, 165, 0);
 
     // Update is called once per frame
     void Update()
     {
         if(healthHolder != playerHealth.RuntimeValue)
         {
+            
             ChangeHealthText();
+            ChangeHealthColour();
         }
     }
 
@@ -22,5 +27,26 @@ public class RL_HealthUI : MonoBehaviour
     {
         displayText.text = (playerHealth.RuntimeValue.ToString() + " / " + playerHealth.InitialValue.ToString());
         healthHolder = playerHealth.RuntimeValue;
+        
+
+
+    }
+
+    //AK: Changing colour by just doing color = new Color(r,g,b) doesn't work for some reason
+    // only seems to work by doing already instantiated values
+    void ChangeHealthColour()
+    {
+        if (playerHealth.RuntimeValue >= playerHealth.InitialValue * 2 / 3)
+        {
+            displayText.color = green;
+        }
+        else if (playerHealth.RuntimeValue >= playerHealth.InitialValue / 2)
+        {
+            displayText.color = orange;
+        }
+        else
+        {
+            displayText.color = red;
+        }
     }
 }
