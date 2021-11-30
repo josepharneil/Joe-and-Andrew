2f71d5f4eac94f0ef88d59067b16831ae3def7f2
andrewklein98 <aklein@qa.com>
2021-11-30 20:20:46 +0000

    organisation

---
+++ grappling hook/Assets/RoguelikeScripts/WorldScripts/RL_TimerManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RL_TimerManager : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    
+
+    /*//public TimerState timerState = TimerState.NotYetActive;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        timer.StartTimer();
+    }
+
+    void Update()
+    {
+        switch (timerState)
+        {
+            case TimerState.NotYetActive:
+                break;
+            case TimerState.Running:
+                //something to run the timer
+                break;
+                //does sometthing to either move the player or reset the timer
+        }
+    }*/
+}
