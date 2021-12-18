3beff86930d2edc3ed0e1d3baaecd74dce79b1a8
andrewklein98 <aklein@qa.com>
2021-12-18 13:49:39 +0000

    Smoothed movement

--- grappling hook/Assets/Scripts/PlayerScripts/AndrewFolder/MoveController.cs
+++ grappling hook/Assets/Scripts/PlayerScripts/AndrewFolder/MoveController.cs
@@ -17,11 +17,23 @@ public class MoveController : MonoBehaviour
 
     BoxCollider2D playerCollider;
     RayCastOrigins raycastOrigins;
+    public CollisionInfo collisions;
 
     struct RayCastOrigins
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+        public void Rest()
+        {
+            above = below = false;
+            left = right = false;
+        }
     }
 
     private void Start()
@@ -33,12 +45,6 @@ public class MoveController : MonoBehaviour
 
     }
 
-    private void Update()
-    {
-        
-        
-      
-    }
 
     void UpdateRaycastOrigins()
     {
@@ -76,23 +82,60 @@ public class MoveController : MonoBehaviour
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
             //adding the velocity x means that the ray is cast from the point where the object will be once it has moved
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+            Debug.DrawRay(rayOrigin,Vector2.up*directionY*rayLength, Color.red);
 
             if (hit)
             {
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 //this stops the rays from hitting something that is further away than the closest collision
                 rayLength = hit.distance;
+
+                collisions.above = directionY == 1;
+                collisions.below = directionY == -1;
             }
         }
     }
 
-    
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        //gets the direction and values of the y velocity
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            //adding the velocity x means that the ray is cast from the point where the object will be once it has moved
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                //this stops the rays from hitting something that is further away than the closest collision
+                rayLength = hit.distance;
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+            }
+        }
+    }
+
+
     public void Move(Vector3 velocity)
     {
-        Debug.Log(velocity.ToString());
+        //Debug.Log(velocity.ToString());
         UpdateRaycastOrigins();
-        VerticalCollisions(ref velocity);
+        collisions.Rest();
+        if (velocity.x != 0)
+        {
+            HorizontalCollisions(ref velocity);
+        }
+        if (velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
         transform.Translate(velocity);
     }
 }
--- grappling hook/Assets/Scripts/PlayerScripts/AndrewFolder/PlayerInputs.cs
+++ grappling hook/Assets/Scripts/PlayerScripts/AndrewFolder/PlayerInputs.cs
@@ -7,21 +7,64 @@ using UnityEngine;
 [RequireComponent(typeof( MoveController))]
 public class PlayerInputs : MonoBehaviour
 {
-    [SerializeField] private float gravity = -20f;
+    [Header("Move Stats")]
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float timeToJumpHeight = 0.4f;
+    [SerializeField] private AnimationCurve accelerationCurve;
+    [SerializeField] [Range(0f,1f)] private float accelerationRate;
+    [SerializeField] private float accelerationTime =0.4f;
+
+    //gravity and jumpVelocity are calculated based on the jump heigth and time
+    private float gravity;
+    private float jumpVelocity;
     Vector3 velocity;
+
+    
+    private float lerpCurrent=0f;
 
     MoveController moveController;
 
     // Start is called before the first frame update
     void Start()
     {
+        gravity = -2 * jumpHeight * Mathf.Pow(timeToJumpHeight, -2);
+        jumpVelocity = timeToJumpHeight * Mathf.Abs(gravity);
         moveController = gameObject.GetComponent<MoveController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (moveController.collisions.above || moveController.collisions.below)
+        {
+            //stop the acceleration due to gravity while in contact with things
+            velocity.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && moveController.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
         velocity.y += gravity * Time.deltaTime;
+
+
+        LerpVelocity(ref velocity.x);
         moveController.Move(velocity * Time.deltaTime);
     }
+
+    void LerpVelocity(ref float velocityX)
+    {
+        float targetVelocityX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (velocityX == 0f || Mathf.Sign(velocityX) != Mathf.Sign(Input.GetAxisRaw("Horizontal")))
+        {
+            lerpCurrent = 0f;
+        }
+        lerpCurrent = Mathf.Lerp(lerpCurrent, 1f, accelerationRate * Time.deltaTime);
+        Debug.Log("Lerp Current: " + lerpCurrent.ToString());
+        velocityX = Mathf.Lerp(velocityX, targetVelocityX, accelerationCurve.Evaluate(lerpCurrent));
+    }
+
 }
