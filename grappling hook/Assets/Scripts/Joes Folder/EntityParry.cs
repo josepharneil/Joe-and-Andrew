26bad26404a69be69f34cb79fcafecce464f24fd
Joseph Arneil <josepharneil@gmail.com>
2022-01-05 20:02:19 +0000

    Parry now has a timer

--- grappling hook/Assets/Scripts/Joes Folder/EntityParry.cs
+++ grappling hook/Assets/Scripts/Joes Folder/EntityParry.cs
@@ -7,9 +7,32 @@ public class EntityParry : MonoBehaviour
     [SerializeField] private PlayerInputs inputs;
     [SerializeField] private LayerMask whatIsParryable;
     [SerializeField] private float parryRadius = 1f;
+    [SerializeField] private float timeBetweenParries = 0.8f;
+    private bool canParry = true;
     
+    private float _parryTimer = 0f;
+
+    private void Update()
+    {
+        if (!canParry)
+        {
+            _parryTimer -= Time.deltaTime;
+            if (_parryTimer <= 0.0f)
+            {
+                canParry = true;
+            }
+        }
+    }
+
     public void CheckParry()
     {
+        if (!canParry)
+        {
+            return;
+        }
+
+        Debug.Log("Parry!");
+        
         FacingDirection parryDirection = inputs.facingDirection;
         
         Vector2 overlapCirclePosition;
@@ -32,14 +55,15 @@ public class EntityParry : MonoBehaviour
         foreach (Collider2D coll in detectedObjects)
         {
             EntityParryable entityParryable = coll.gameObject.GetComponent<EntityParryable>();
-            if (entityParryable && entityParryable.isCurrentlyParryable)
-            {
-                entityParryable.Parry();
-                break;
-            }
+            if (!entityParryable || !entityParryable.isCurrentlyParryable) continue;
+            entityParryable.Parry();
+            break;
 
             // Instantiate a hit particle here if we want
         }
+
+        canParry = false;
+        _parryTimer = timeBetweenParries;
     }
 
     private void OnDrawGizmos()
