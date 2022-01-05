5572d16efdfa3b26150726636bd447e3cbdc26e2
Joseph Arneil <josepharneil@gmail.com>
2022-01-05 22:59:43 +0000

    New spiky property - damages player when touched

--- grappling hook/Assets/Scripts/Joes Folder/EntityKnockback.cs
+++ grappling hook/Assets/Scripts/Joes Folder/EntityKnockback.cs
@@ -6,16 +6,16 @@ public class EntityKnockback : MonoBehaviour
     /// <summary>
     /// This probably should take into account the "weight" of the entity, ie. heavier entities get knocked back less.
     /// </summary>
-    /// <param name="hitDirection"></param>
+    /// <param name="knockbackDirection"></param>
     /// <param name="knockbackStrength"></param>
     /// <param name="duration"></param>
-    public void Knockback(Vector3 hitDirection, float knockbackStrength, float duration)
+    public void Knockback(Vector3 knockbackDirection, float knockbackStrength, float duration)
     {
-        hitDirection.Normalize();
-        hitDirection.y = 0f;
-        hitDirection.z = 0f;
+        knockbackDirection.Normalize();
+        knockbackDirection.y = 0f;
+        knockbackDirection.z = 0f;
         
-        transform.DOMove(transform.position + (hitDirection*knockbackStrength ), duration).SetEase(Ease.OutCubic);
+        transform.DOMove(transform.position + (knockbackDirection*knockbackStrength ), duration).SetEase(Ease.OutCubic);
     }
 
 }
