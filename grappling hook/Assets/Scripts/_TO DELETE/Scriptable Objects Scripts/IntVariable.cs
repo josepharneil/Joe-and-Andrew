c6ae49761afc4587112dc72b08a728ab6f2ae848
Joseph Arneil <josepharneil@gmail.com>
2022-01-13 15:13:44 +0000

    Big restructure into TO DELETE folders

--- grappling hook/Assets/Scripts/_TO DELETE/Scriptable Objects Scripts/IntVariable.cs
+++ grappling hook/Assets/Scripts/_TO DELETE/Scriptable Objects Scripts/IntVariable.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+//NOTE: I've removed this for now because we don't need it.
+//[CreateAssetMenu]
 public class IntVariable : ScriptableObject, ISerializationCallbackReceiver
 {
     public int InitialValue;
