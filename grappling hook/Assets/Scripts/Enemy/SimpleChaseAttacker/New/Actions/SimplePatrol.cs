38995798fb45b5be568d74dadaa3d307bcbf73f3
Joseph Arneil <josepharneil@gmail.com>
2022-01-25 17:56:44 +0000

    Improved simple patrol script

--- grappling hook/Assets/Scripts/Enemy/SimpleChaseAttacker/New/Actions/SimplePatrol.cs
+++ grappling hook/Assets/Scripts/Enemy/SimpleChaseAttacker/New/Actions/SimplePatrol.cs
@@ -1,5 +1,6 @@
 using Entity;
 using UnityEngine;
+using Utilities;
 
 namespace AI
 {
@@ -12,19 +13,19 @@ namespace AI
         // between two points, dest point must be to the right.
         // patrols from start point to dest point to right.
         
-        private Vector3 _startPoint;
-        [SerializeField] private Transform destinationPoint;
+        [SerializeField] private Transform patrolPoint0;
+        [SerializeField] private Transform patrolPoint1;
 
+        private int _currentTargetPatrolPointIndex;
         private FacingDirection _facingDirection = FacingDirection.Right;
 
         private const float Speed = 2f;
 
         private void Start()
         {
-            _startPoint = transform.localPosition;
-            Debug.Assert(_startPoint.x < destinationPoint.localPosition.x, 
-                "This very simple script only allows a patrol destination towards the right of the initial position.", 
-                this);
+            Debug.Assert(patrolPoint0 != null, this);
+            Debug.Assert(patrolPoint1 != null, this);
+            _currentTargetPatrolPointIndex = 0;
         }
 
         public override void UpdatePatrol()
@@ -34,23 +35,60 @@ namespace AI
 
         private void MoveTowardsDestination()
         {
-            if (_facingDirection == FacingDirection.Right)
+            Transform targetPatrolPoint = UpdateTargetPatrolPoint();
+            
+            UpdateFacingDirection(targetPatrolPoint);
+            
+            MoveInDirection();
+        }
+
+        private Transform UpdateTargetPatrolPoint()
+        {
+            Transform targetPatrolPoint = _currentTargetPatrolPointIndex == 0 ? patrolPoint0 : patrolPoint1;
+
+            // If we're close enough to our destination point, update the destination
+            const float distanceThreshold = 0.1f;
+            if (Vector2.Distance(transform.position, targetPatrolPoint.position) < distanceThreshold)
             {
-                if (Vector3.Distance(transform.localPosition, destinationPoint.localPosition) < 0.1f)
-                {
-                    _facingDirection = FacingDirection.Left;
-                }
-            }
-            else
-            {
-                if (Vector3.Distance(transform.localPosition, _startPoint) < 0.1f)
-                {
-                    _facingDirection = FacingDirection.Right;
-                }
+                _currentTargetPatrolPointIndex = (_currentTargetPatrolPointIndex + 1) % 2;
+                targetPatrolPoint = _currentTargetPatrolPointIndex == 0 ? patrolPoint0 : patrolPoint1;
             }
 
+            return targetPatrolPoint;
+        }
+
+        private void UpdateFacingDirection(Transform currentTargetPatrolPoint)
+        {
+            // Update facing direction
+            if (transform.position.IsRightOf(currentTargetPatrolPoint.position))
+            {
+                _facingDirection = FacingDirection.Left;
+            }
+            else if (transform.position.IsLeftOf(currentTargetPatrolPoint.position))
+            {
+                _facingDirection = FacingDirection.Right;
+            }
+        }
+
+        private void MoveInDirection()
+        {
+            // Move
             float fallSpeed = !movement.customCollider2D.GetCollisionBelow() ? Physics2D.gravity.y : 0f;
             movement.MoveAtSpeed(new Vector2((float)_facingDirection * Speed, fallSpeed));
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!patrolPoint0 || !patrolPoint1) return;
+            
+            var patrolPoint0Position = patrolPoint0.position;
+            var patrolPoint1Position = patrolPoint1.position;
+                
+            const float wireSphereRadius = 0.5f;
+            Gizmos.DrawWireSphere(patrolPoint0Position, wireSphereRadius);
+            Gizmos.DrawWireSphere(patrolPoint1Position, wireSphereRadius);
+                
+            Gizmos.DrawLine(patrolPoint0Position, patrolPoint1Position);
         }
     }
 }
---
+++ grappling hook/Assets/Scripts/Utilities/Extensions.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class Extensions
+    {
+        // public static Vector3 AsVector3(this Vector2 v2)
+        // {
+        //     return v2;
+        // }
+
+        // public static float DistanceSquared(this Vector2 v2)
+        // {
+        //     
+        //     return (v2.x * v2.x) + (v2.y * v2.y);
+        // }
+        //
+        // public static float DistanceSquared(this Vector3 v3)
+        // {
+        //     return (v3.x * v3.x) + (v3.y * v3.y) + (v3.z * v3.z);
+        // }
+        
+        // public static float DistanceSquared(this Vector2 a, Vector2 b)
+        // {
+        //     float num1 = a.x - b.x;
+        //     float num2 = a.y - b.y;
+        //     return (float) ((double) num1 * (double) num1 + (double) num2 * (double) num2);
+        // }
+
+        public static bool IsRightOf(this Vector3 v0, Vector3 v1)
+        {
+            return v0.x > v1.x;
+        }
+        public static bool IsLeftOf(this Vector3 v0, Vector3 v1)
+        {
+            return v0.x < v1.x;
+        }
+        public static bool IsAbove(this Vector3 v0, Vector3 v1)
+        {
+            return v0.y > v1.y;
+        }
+        public static bool IsBelow(this Vector3 v0, Vector3 v1)
+        {
+            return v0.y < v1.y;
+        }
+
+        public static bool IsRightOf(this Vector2 v0, Vector2 v1)
+        {
+            return v0.x > v1.x;
+        }
+        public static bool IsLeftOf(this Vector2 v0, Vector2 v1)
+        {
+            return v0.x < v1.x;
+        }
+        public static bool IsAbove(this Vector2 v0, Vector2 v1)
+        {
+            return v0.y > v1.y;
+        }
+        public static bool IsBelow(this Vector2 v0, Vector2 v1)
+        {
+            return v0.y < v1.y;
+        }
+
+        
+        
+    }
+}
