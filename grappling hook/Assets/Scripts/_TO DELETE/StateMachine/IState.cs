d2c2da0c7148cf57307961e63b67023a3a08bfc8
Joseph Arneil <josepharneil@gmail.com>
2022-02-03 13:40:55 +0000

    Deletion of old stuff

--- grappling hook/Assets/Scripts/_TO DELETE/StateMachine/IState.cs
+++
@@ -1,9 +0,0 @@
-﻿// A simple interface defining what a State is for the StateMchine.
-public interface IState
-{
-    void OnEnter(); // Upon entering this state, what do we do?
-    void Tick(); // What to do on a tick update. (A tick is one call of the Unity function Update)
-    void FixedTick(); // What to do on a fixedtick update. (A tick is one call of the Unity function FixedUpdate)
-    
-    void OnExit(); // Upon exiting this state, what do we do?
-}
