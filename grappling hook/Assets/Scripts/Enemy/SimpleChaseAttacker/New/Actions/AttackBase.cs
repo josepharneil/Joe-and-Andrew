713cc55c711a0a10ecf2c4db21b0566a5f0cf9f5
Joseph Arneil <josepharneil@gmail.com>
2022-02-03 14:28:33 +0000

    Loads of deletion

--- grappling hook/Assets/Scripts/Enemy/SimpleChaseAttacker/New/Actions/AttackBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace AI
-{
-    public abstract class AttackBase : MonoBehaviour
-    {
-        public abstract void UpdateAttack();
-    }
-}
--- grappling hook/Assets/Scripts/Enemy/SimpleChaseAttacker/New/Actions/ChaseBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace AI
-{
-    public abstract class ChaseBase : MonoBehaviour
-    {
-        public abstract void UpdateChase();
-    }
-}
