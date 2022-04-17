5229e8251e83d1be0af4896ddfc4b6ebf3038ff3
Joseph Arneil <josepharneil@gmail.com>
2022-04-17 20:44:14 +0100

    Made ok progress on boss AI

--- grappling hook/Assets/Scripts/Enemy/DemoBoss/DemoBossEnemyController.cs
+++ grappling hook/Assets/Scripts/Enemy/DemoBoss/DemoBossEnemyController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Entity;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 namespace Enemy
@@ -8,18 +9,68 @@ namespace Enemy
     public class DemoBossEnemyController : MonoBehaviour
     {
         [Header("Components")]
-        [HideInInspector] public MovementController MovementController;
-        [SerializeField] private BoxCollider2D arenaBox;
-        [HideInInspector] public GameObject target;
+        [SerializeField] private MovementController _movementController;
+        [SerializeField] private BoxCollider2D _arenaBox;
+        [SerializeField] private GameObject _target;
 
         // [] charging
-        private Vector3 _chargeTargetPosition;
-        [SerializeField] private float _chargeSpeed = 5f;
-        [SerializeField] private float _chargeDuration = 2f;
+        private Vector2 _chargeDestination;
+        [SerializeField] private float _chargeWindUpDuration = 1f;
+        [SerializeField] private float _chargeSpeed = 10f;
+        [SerializeField] private float _chargeDistance = 5f;
+        [SerializeField] private float _chargeRecoveryDuration = 0.5f;
+
+        private void Update()
+        {
+            _movementController.Move(Vector2.down * 9.81f);
+        }
+
+        public float GetChargeWindUpDuration()
+        {
+            return _chargeWindUpDuration;
+        }
+
+        public float GetChargeRecoveryDuration()
+        {
+            return _chargeRecoveryDuration;
+        }
 
         public bool TargetIsInBossArea()
         {
-            return arenaBox.bounds.Contains(target.transform.position);
+            return _arenaBox.bounds.Contains(_target.transform.position);
         }
+
+        public void SetChargeDestination()
+        {
+            Vector2 thisPosition = transform.position;
+            Vector2 directionNormToTarget = thisPosition.DirectionToNormalized(_target.transform.position);
+            RaycastHit2D hit = Physics2D.Raycast(thisPosition, directionNormToTarget, _chargeDistance, _movementController.customCollider2D.GetCollisionMask().value);
+            if (hit)
+            {
+                _chargeDestination = hit.point - (directionNormToTarget * 3f);
+            }
+            else
+            {
+                _chargeDestination = thisPosition + (directionNormToTarget * _chargeDistance);
+            }
+        }
+
+        public bool IsChargeAttackDone()
+        {
+            return transform.position.DistanceToSquared(_chargeDestination) < Mathf.Pow(1f, 2);
+        }
+
+        public void ChargeAttack()
+        {
+            _movementController.Move(transform.position.DirectionToNormalized(_chargeDestination) * _chargeSpeed);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawLine(transform.position, _chargeDestination);
+            Gizmos.DrawWireSphere(_chargeDestination, 0.2f);
+        }
+#endif
     }
 }
